037cc6dfee20ce31059f7506d0ef3c0d199b0469
ozerkunal <ozer@unal-group.com>
2022-09-27 12:53:48 +0300

    Add project files.

---
+++ src/kodlama.io.Devs/Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageCommandValidator : AbstractValidator<UpdateProgrammingLanguageCommand>
+    {
+        public UpdateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(c => c.LanguageName).NotEmpty().WithMessage("Language Name Can Not Be Empty");
+            RuleFor(c => c.Id).GreaterThan(0).WithMessage("Id Must Be Greater Than 0");
+            RuleFor(c => c.Id).NotEmpty();
+        }
+    }
+}
---
+++ src/kodlama.io.Devs/Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,34 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCanNotBeDuplicated(string languageName)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(b => b.LanguageName == languageName);
+            if (result.Items.Any()) throw new BusinessException("Programming Language name already exists.");
+        }
+
+        public void ProgrammingLanguageShouldExistWhenRequested(ProgrammingLanguage programmingLanguage)
+        {
+            if (programmingLanguage == null) throw new BusinessException("Requested Data does not exists.");
+        }
+
+    }
+}
